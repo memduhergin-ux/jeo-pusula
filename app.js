14431327f1bd1b2b839af815b476cee5cd4d6678
memduhergin-ux <memduhergin@gmail.com>
2026-02-17 13:45:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿const APP_VERSION = 'v1453-35F'; // Performance Optimization & Smooth Drag Fix
+﻿const APP_VERSION = 'v1453-36F'; // Grid Panel Position Fix
 const JEO_VERSION = APP_VERSION; // Geriye dönük uyumluluk için
 const DB_NAME = 'jeo_pusulasi_db';
 const JEO_DB_VERSION = 1;
